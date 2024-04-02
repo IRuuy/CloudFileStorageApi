db9d51eaf7cbd37689efcafbd33f45b85480ffb8
Daniil Shulgin <manganeko@yandex.ru>
2024-04-02 14:19:28 +0700

    chore(spring): add base init code from spring initializer

---
+++ src/test/java/ru/shulgindaniil/cloudFileStorage/CloudFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shulgindaniil.cloudFileStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
