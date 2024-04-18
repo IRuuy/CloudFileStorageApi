56a2c59a451b820069644a457fa8e84e581c7fcf
Daniil Shulgin <manganeko@yandex.ru>
2024-04-18 23:27:47 +0700

    chore(mapper): add method for map collections of objects

--- src/main/java/ru/shulgindaniil/cloudFileStorage/common/Mappable.java
+++ src/main/java/ru/shulgindaniil/cloudFileStorage/common/Mappable.java
@@ -1,5 +1,9 @@
 package ru.shulgindaniil.cloudFileStorage.common;
+
+import java.util.Collection;
+
 public interface Mappable<E, D> {
     D toDto(E entity);
     E toEntity(D dto);
+    Collection<D> toDto(Collection<E> entities);
 }
