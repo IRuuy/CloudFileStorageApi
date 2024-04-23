c0fac5001e033b75a147116152df00165c62dd54
Daniil Shulgin <manganeko@yandex.ru>
2024-04-23 22:49:54 +0700

    feat(exception): add common exception`ResourseNotFoundException`

--- src/main/java/ru/shulgindaniil/cloudFileStorage/common/exception/ResourceNotFoundException.java
+++ src/main/java/ru/shulgindaniil/cloudFileStorage/common/exception/ResourceNotFoundException.java
@@ -4,4 +4,7 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException() {
         super("Resource not found.");
     }
+    public ResourceNotFoundException(String id) {
+        super(String.format("Resource with id - %s not found.", id));
+    }
 }
