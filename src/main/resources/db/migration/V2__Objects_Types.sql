e36cebf56be452eeab3b6a1dc1974d6b4a561e65
Daniil Shulgin <manganeko@yandex.ru>
2024-04-23 22:42:11 +0700

    refactor(migration): denormalization table `file_object_type`
    Denormalization is justified by the fact that the number of object types is constant and is always named in the same style. This reduces the load on the database.

--- src/main/resources/db/migration/V2__Objects_Types.sql
+++ src/main/resources/db/migration/V2__Objects_Types.sql
@@ -1,22 +1,14 @@
-CREATE TABLE file_object_type (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR
-);
-
 CREATE TABLE file_object (
     id VARCHAR PRIMARY KEY,
     parent_id VARCHAR,
-    type_id INTEGER,
     owner_id VARCHAR,
+    type VARCHAR,
     name VARCHAR,
     created_at TIMESTAMP DEFAULT NOW(),
     modified_at TIMESTAMP DEFAULT NOW(),
     size BIGINT DEFAULT 0,
 
     CONSTRAINT fk_parent_file_object FOREIGN KEY (parent_id) REFERENCES file_object(id) ON DELETE CASCADE,
-    CONSTRAINT fk_type_file_object FOREIGN KEY (type_id) REFERENCES file_object_type(id),
-    CONSTRAINT fk_owner_file_object FOREIGN KEY (owner_id) REFERENCES "user"(id)
+    CONSTRAINT fk_owner_file_object FOREIGN KEY (owner_id) REFERENCES "user"(id),
+    CONSTRAINT unique_object UNIQUE(parent_id, type, name)
 );
-
-INSERT INTO file_object_type (name)
-VALUES ('file'), ('folder');
